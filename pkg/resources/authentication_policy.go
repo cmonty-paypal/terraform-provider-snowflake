0f75aebef33edd8b62654991e3c6b7ba6aea5ef4
cmonty <cmonty@paypal.com>
2024-07-20 22:20:28 +0000

    more functionality

--- pkg/resources/authentication_policy.go
+++ pkg/resources/authentication_policy.go
@@ -1,6 +1,11 @@
 package resources
 
 import (
+	"context"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/helpers"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/provider"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -114,3 +119,104 @@ func AuthenticationPolicy() *schema.Resource {
 		},
 	}
 }
+
+// CreateAuthenticationPolicy implements schema.CreateFunc.
+func CreateAuthenticationPolicy(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*provider.Context).Client
+	ctx := context.Background()
+	name := d.Get("name").(string)
+	database := d.Get("database").(string)
+	schema := d.Get("schema").(string)
+	objectIdentifier := sdk.NewSchemaObjectIdentifier(database, schema, name)
+
+	createOptions := &sdk.CreateAuthenticationPolicyRequest{
+		name: objectIdentifier,
+		OrReplace:                 sdk.Bool(d.Get("or_replace").(bool)),
+		AuthenticationMethods: d.Get("authentication_methods").([]sdk.AuthenticationMethods),
+		MfaAuthenticationMethods: d.Get("mfa_authentication_methods").([]sdk.MfaAuthenticationMethods),
+		MfaEnrollment: sdk.String(d.Get("mfa_enrollment").(string)),
+		ClientTypes: d.Get("client_types").([]sdk.ClientTypes),
+		SecurityIntegrations: d.Get("security_integrations").([]sdk.SecurityIntegrationsOption),
+	}
+
+	if v, ok := d.GetOk("comment"); ok {
+		createOptions.Comment = sdk.String(v.(string))
+	}
+
+	err := client.AuthenticationPolicies.Create(ctx, createOptions)
+	if err != nil {
+		return err
+	}
+	d.SetId(helpers.EncodeSnowflakeID(objectIdentifier))
+	return ReadAuthenticationPolicy(d, meta)
+}
+
+// ReadAuthenticationPolicy implements schema.ReadFunc.
+func ReadAuthenticationPolicy(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*provider.Context).Client
+	ctx := context.Background()
+	objectIdentifier := helpers.DecodeSnowflakeID(d.Id()).(sdk.SchemaObjectIdentifier)
+
+	if err := d.Set("qualified_name", objectIdentifier.FullyQualifiedName()); err != nil {
+		return err
+	}
+
+	authenticationPolicy, err := client.AuthenticationPolicies.ShowByID(ctx, objectIdentifier)
+	if err != nil {
+		return err
+	}
+
+	if err := d.Set("database", authenticationPolicy.DatabaseName); err != nil {
+		return err
+	}
+	if err := d.Set("schema", authenticationPolicy.SchemaName); err != nil {
+		return err
+	}
+	if err := d.Set("name", authenticationPolicy.Name); err != nil {
+		return err
+	}
+	if err := d.Set("comment", authenticationPolicy.Comment); err != nil {
+		return err
+	}
+	// passwordPolicyDetails, err := client.PasswordPolicies.Describe(ctx, objectIdentifier)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// if err := setIntProperty(d, "min_length", passwordPolicyDetails.PasswordMinLength); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "max_length", passwordPolicyDetails.PasswordMaxLength); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "min_upper_case_chars", passwordPolicyDetails.PasswordMinUpperCaseChars); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "min_lower_case_chars", passwordPolicyDetails.PasswordMinLowerCaseChars); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "min_numeric_chars", passwordPolicyDetails.PasswordMinNumericChars); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "min_special_chars", passwordPolicyDetails.PasswordMinSpecialChars); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "min_age_days", passwordPolicyDetails.PasswordMinAgeDays); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "max_age_days", passwordPolicyDetails.PasswordMaxAgeDays); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "max_retries", passwordPolicyDetails.PasswordMaxRetries); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "lockout_time_mins", passwordPolicyDetails.PasswordLockoutTimeMins); err != nil {
+	// 	return err
+	// }
+	// if err := setIntProperty(d, "history", passwordPolicyDetails.PasswordHistory); err != nil {
+	// 	return err
+	// }
+
+	return nil
+}
+
